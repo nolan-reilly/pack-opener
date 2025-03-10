038142a657c5e05a4fbb55baf977fb5010d28572
Nolan Reilly <nolanreilly02@gmail.com>
2025-03-09 20:47:06 -0500

    Create title, description and score content

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,50 @@ import Pokemon from "./Pokemon";
 
 function App() {
   return (
-    <div className="container">
-      <div className="grid-container">
-        <Pokemon name="mewtwo" />
-        <Pokemon name="pikachu" />
-        <Pokemon name="suicune" />
-        <Pokemon name="charizard" />
-        <Pokemon name="rayquaza" />
+    <>
+      <div className="container">
+        <div className="flex flex-row space-between">
+          <div className="margin-bottom-24 flex flex-col gap-16">
+            <h1>Pokemon Memory Game</h1>
+            <p>
+              Get points by clicking on an image but don't click on any more
+              than once!
+            </p>
+          </div>
 
-        <Pokemon name="gengar" />
-        <Pokemon name="blaziken" />
-        <Pokemon name="greninja" />
-        <Pokemon name="garchomp" />
-        <Pokemon name="decidueye" />
+          <div>
+            <p>Score: 0</p>
+            <p>Best Score: 0</p>
+          </div>
+        </div>
 
-        <Pokemon name="metagross" />
-        <Pokemon name="wobbuffet" />
-        <Pokemon name="arcanine" />
-        <Pokemon name="snorlax" />
-        <Pokemon name="venusaur" />
+        <div className="grid-container">
+          <Pokemon name="mewtwo" />
+          <Pokemon name="pikachu" />
+          <Pokemon name="suicune" />
+          <Pokemon name="charizard" />
+          <Pokemon name="rayquaza" />
 
-        <Pokemon name="dialga" />
-        <Pokemon name="nidoking" />
-        <Pokemon name="dragonite" />
-        <Pokemon name="alakazam" />
-        <Pokemon name="torterra" />
+          <Pokemon name="gengar" />
+          <Pokemon name="blaziken" />
+          <Pokemon name="greninja" />
+          <Pokemon name="garchomp" />
+          <Pokemon name="decidueye" />
+
+          <Pokemon name="metagross" />
+          <Pokemon name="wobbuffet" />
+          <Pokemon name="arcanine" />
+          <Pokemon name="snorlax" />
+          <Pokemon name="venusaur" />
+
+          <Pokemon name="dialga" />
+          <Pokemon name="nidoking" />
+          <Pokemon name="dragonite" />
+          <Pokemon name="alakazam" />
+          <Pokemon name="torterra" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
